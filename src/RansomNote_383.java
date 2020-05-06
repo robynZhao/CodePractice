e50de7a4c7a9b6e75de36b279a86b8578d046829
robynZhao <zhaorobyn895@gmail.com>
2020-05-06 14:23:06 -0500

    upload Q-383

--- src/RansomNote_383.java
+++ src/RansomNote_383.java
@@ -1,25 +1,42 @@
 import java.util.*;
-import java.util.Hashtable;
 
 public class RansomNote_383 {
     public static void main(String[] args) {
-        canConstruct("a", "bbcddfd");
+        String ransomNote = "a";
+        String magazine = "bbcddfd";
+        boolean result = canConstruct(ransomNote, magazine);
+        System.out.println(result);
     }
 
     public static boolean canConstruct(String ransomNote, String magazine) {
         // store magazine to a hashtable
-        Hashtable<String, Integer> hashMagazine = new Hashtable<String, Integer>();
+        HashMap<Character, Integer> hashMagazine = new HashMap<Character, Integer>();
         for(int i = 0; i < magazine.length(); i ++) {
-            if(hashMagazine.containsKey(magazine.charAt(i) + "")) {
-                int tempValue = hashMagazine.get(magazine.charAt(i) + "") + 1;
-                System.out.println("tempValue is: " + tempValue);
-                hashMagazine.put(magazine.charAt(i) + "", tempValue);
+            if(hashMagazine.containsKey(magazine.charAt(i))) {
+                int tempValue = hashMagazine.get(magazine.charAt(i)) + 1;
+                hashMagazine.put(magazine.charAt(i), tempValue);
             } else {
-                hashMagazine.put(magazine.charAt(i) + "", 1);
+                hashMagazine.put(magazine.charAt(i), 1);
             }
         }
-        System.out.println(hashMagazine);
+        // System.out.println(hashMagazine);
 
-        return false;
+        for(int i = 0; i < ransomNote.length(); i ++) {
+            if(!hashMagazine.containsKey(ransomNote.charAt(i))) {
+                return false;
+            } else {
+                int tempValue = hashMagazine.get(ransomNote.charAt(i)) - 1;
+
+                if(tempValue < 0) {
+                    return false;
+                } else if(tempValue == 0) {
+                    hashMagazine.remove(ransomNote.charAt(i));
+                } else {
+                    hashMagazine.put(ransomNote.charAt(i), tempValue);
+                }
+            }
+        }
+
+        return true;
     }
 }
